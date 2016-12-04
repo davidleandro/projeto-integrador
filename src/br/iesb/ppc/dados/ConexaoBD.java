037d2143e43c29e9b6d225b9a5047541e8b74fa0
Unknown <tom.robson@hotmail.com>
2016-12-04 11:03:18 -0200

    Otimizado ConexaoBD

--- src/br/iesb/ppc/dados/ConexaoBD.java
+++ src/br/iesb/ppc/dados/ConexaoBD.java
@@ -3,13 +3,22 @@ package br.iesb.ppc.dados;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public final class ConexaoBD {
     
     public static Connection getConexao() throws DadosException {
+        String jdbc = "jdbc:mysql://localhost:3306/mydb";
+        
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "iesb");
+        properties.setProperty("useSSL", "false");
+        properties.setProperty("autoReconnect", "true");
+        
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "iesb");
+            return DriverManager.getConnection(jdbc, properties);
         } catch (ClassNotFoundException e) {
              System.out.println(e.getMessage());
             throw new DadosException("Erro ao carregar JDBC: " + e.getMessage());
