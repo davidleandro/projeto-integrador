87806af15a95b238548915e2992d5d8d3aa04627
Luan da Costa <luandacostadf@gmail.com>
2016-12-07 17:10:52 -0200

    Entidade professor

--- src/br/iesb/ppc/entidade/Professor.java
+++ src/br/iesb/ppc/entidade/Professor.java
@@ -4,146 +4,70 @@ import java.io.Serializable;
 import java.sql.Date;
 import java.util.Objects;
 
-public class Ata implements Serializable {
+public class Professor implements Serializable {
+    int id;
+    String nome;
+    int cpf;
+    String maiorTitulacao;
+    String formacaoTitulacao;
+    String curriculoLates;
+    String atualizacaoCurriculo;
 
-    private String data;
-    private String horaInicio;
-    private String horaFim;
-    private String assunto;
-    private String sede;
-    private String sala;
-    private String descricao;
-    private int id;
-
-    public Ata(String data, String horaInicio, String horaFim, String assunto, String sede, String sala, String descricao) {
-        this.data = data;
-        this.horaInicio = horaInicio;
-        this.horaFim = horaFim;
-        this.assunto = assunto;
-        this.sede = sede;
-        this.sala = sala;
-        this.descricao = descricao;
-    }
-
-    public Ata() {
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public String getHoraInicio() {
-        return horaInicio;
-    }
-
-    public void setHoraInicio(String horaInicio) {
-        this.horaInicio = horaInicio;
-    }
-
-    public String getHoraFim() {
-        return horaFim;
-    }
-
-    public void setHoraFim(String horaFim) {
-        this.horaFim = horaFim;
-    }
-
-    public String getAssunto() {
-        return assunto;
-    }
-
-    public void setAssunto(String assunto) {
-        this.assunto = assunto;
-    }
-
-    public String getSede() {
-        return sede;
-    }
-
-    public void setSede(String sede) {
-        this.sede = sede;
-    }
-
-    public String getSala() {
-        return sala;
-    }
-
-    public void setSala(String sala) {
-        this.sala = sala;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public int getId() {
+        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public int getId() {
-        return id;
+    public String getNome() {
+        return nome;
     }
 
-    @Override
-    public String toString() {
-        return "Ata{" + "data=" + data + ", horaInicio=" + horaInicio + ", horaFim=" + horaFim + ", assunto=" + assunto + ", sede=" + sede + ", sala=" + sala + ", descricao=" + descricao + '}';
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 37 * hash + Objects.hashCode(this.data);
-        hash = 37 * hash + Objects.hashCode(this.horaInicio);
-        hash = 37 * hash + Objects.hashCode(this.horaFim);
-        hash = 37 * hash + Objects.hashCode(this.assunto);
-        hash = 37 * hash + Objects.hashCode(this.sede);
-        hash = 37 * hash + Objects.hashCode(this.sala);
-        hash = 37 * hash + Objects.hashCode(this.descricao);
-        return hash;
+    public int getCpf() {
+        return cpf;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Ata other = (Ata) obj;
-        if (!Objects.equals(this.horaInicio, other.horaInicio)) {
-            return false;
-        }
-        if (!Objects.equals(this.horaFim, other.horaFim)) {
-            return false;
-        }
-        if (!Objects.equals(this.assunto, other.assunto)) {
-            return false;
-        }
-        if (!Objects.equals(this.sede, other.sede)) {
-            return false;
-        }
-        if (!Objects.equals(this.sala, other.sala)) {
-            return false;
-        }
-        if (!Objects.equals(this.descricao, other.descricao)) {
-            return false;
-        }
-        if (!Objects.equals(this.data, other.data)) {
-            return false;
-        }
-        return true;
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
     }
 
+    public String getMaiorTitulacao() {
+        return maiorTitulacao;
+    }
+
+    public void setMaiorTitulacao(String maiorTitulacao) {
+        this.maiorTitulacao = maiorTitulacao;
+    }
+
+    public String getFormacaoTitulacao() {
+        return formacaoTitulacao;
+    }
+
+    public void setFormacaoTitulacao(String formacaoTitulacao) {
+        this.formacaoTitulacao = formacaoTitulacao;
+    }
+
+    public String getCurriculoLates() {
+        return curriculoLates;
+    }
+
+    public void setCurriculoLates(String curriculoLates) {
+        this.curriculoLates = curriculoLates;
+    }
+
+    public String getAtualizacaoCurriculo() {
+        return atualizacaoCurriculo;
+    }
+
+    public void setAtualizacaoCurriculo(String atualizacaoCurriculo) {
+        this.atualizacaoCurriculo = atualizacaoCurriculo;
+    }
+    
+    
 }
